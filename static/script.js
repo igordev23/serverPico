1fd1d42ea87fdbd9d774ac69748675cd87a7d038
igordev23 <igor.dev89898@gmail.com>
2025-04-03 11:39:28 -0300

    colocado ver logs e ver busssola e ejuatsando erero na bussloa commlogs ok

--- static/script.js
+++ static/script.js
@@ -4,6 +4,7 @@ function atualizarMensagens() {
         .then(data => {
             let mensagensDiv = document.getElementById('mensagens');
             mensagensDiv.innerHTML = '';
+
             if (data.length > 0) {
                 data.forEach(msg => {
                     let div = document.createElement('div');
@@ -11,13 +12,15 @@ function atualizarMensagens() {
                     div.textContent = msg;
                     mensagensDiv.appendChild(div);
                 });
+
+                // Atualiza a bússola com a última mensagem recebida
+                let ultimaMensagem = data[data.length - 1];
+                atualizarBussola(ultimaMensagem);
             } else {
                 mensagensDiv.innerHTML = '<p>Nenhuma mensagem recebida ainda.</p>';
             }
         });
 }
-
-
 
 function atualizarBussola(direcao) {
     let angulos = {
@@ -30,26 +33,28 @@ function atualizarBussola(direcao) {
         "Oeste": 270,
         "Noroeste": 315
     };
-    let angulo = angulos[direcao] || 0;
-    document.getElementById('ponteiro').style.transform = `translate(-50%, -100%) rotate(${angulo}deg)`;
+    
+    // Apenas atualiza a bússola se a direção recebida for válida
+    if (angulos.hasOwnProperty(direcao)) {
+        let angulo = angulos[direcao];
+        document.getElementById('ponteiro').style.transform = `translate(-50%, -100%) rotate(${angulo}deg)`;
+    }
 }
+
 function resetarBussola() {
     document.getElementById('ponteiro').style.transform = `translate(-50%, -100%) rotate(0deg)`;
 }
 
+function resetarMensagens() {
+    fetch('/reset', { method: 'POST' })
+        .then(() => atualizarMensagens());
+}
 
 function mostrarSeção(selecao) {
     document.getElementById('compass-container').style.display = 'none';
     document.getElementById('logs-container').style.display = 'none';
 
     document.getElementById(selecao).style.display = 'block';
-}
-function resetarMensagens() {
-    fetch('/reset', { method: 'POST' })
-        .then(() => {
-            atualizarMensagens();
-            resetarBussola();
-        });
 }
 
 setInterval(atualizarMensagens, 2000); // Atualiza os logs a cada 2s
