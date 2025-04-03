729defe84b0b4955b0237b75d200079a6bc37af2
igordev23 <igor.dev89898@gmail.com>
2025-04-03 11:10:13 -0300

    colocado ver logs e ver busssola

--- static copy/script.js
+++ static copy/script.js
@@ -23,31 +23,18 @@ function atualizarBussola(direcao) {
     let angulo = angulos[direcao] || 0;
     document.getElementById('ponteiro').style.transform = `translate(-50%, -100%) rotate(${angulo}deg)`;
 }
-
 function resetarBussola() {
     document.getElementById('ponteiro').style.transform = `translate(-50%, -100%) rotate(0deg)`;
 }
 
-function mostrarSeção(selecao) {
-    document.getElementById('compass-container').style.display = 'none';
-    document.getElementById('logs-container').style.display = 'none';
 
-    document.getElementById(selecao).style.display = 'block';
-}
-
-function atualizarLogs() {
-    fetch('/logs')
-        .then(response => response.json())
-        .then(data => {
-            let logsDiv = document.getElementById('logs');
-            logsDiv.innerHTML = '';
-            data.forEach(log => {
-                let p = document.createElement('p');
-                p.textContent = log;
-                logsDiv.appendChild(p);
-            });
+function resetarMensagens() {
+    fetch('/reset', { method: 'POST' })
+        .then(() => {
+            atualizarMensagens();
+            resetarBussola();
         });
 }
 
-setInterval(atualizarMensagens, 2000); // Atualiza a bússola a cada 2s
-setInterval(atualizarLogs, 2000); // Atualiza os logs a cada 2s
+
+setInterval(atualizarMensagens, 2000); // Atualiza a cada 2 segundos
