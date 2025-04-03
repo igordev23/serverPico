ceabaf234dfde0bf09a30712fdb08a93c132d47f
igordev23 <igor.dev89898@gmail.com>
2025-04-03 11:34:09 -0300

    colocado ver logs e ver busssola e ejuatsando erero na bussloa commlogs ok

--- static/script.js
+++ static/script.js
@@ -17,10 +17,7 @@ function atualizarMensagens() {
         });
 }
 
-function resetarMensagens() {
-    fetch('/reset', { method: 'POST' })
-        .then(() => atualizarMensagens());
-}
+
 
 function atualizarBussola(direcao) {
     let angulos = {
@@ -36,16 +33,23 @@ function atualizarBussola(direcao) {
     let angulo = angulos[direcao] || 0;
     document.getElementById('ponteiro').style.transform = `translate(-50%, -100%) rotate(${angulo}deg)`;
 }
-
 function resetarBussola() {
     document.getElementById('ponteiro').style.transform = `translate(-50%, -100%) rotate(0deg)`;
 }
+
 
 function mostrarSeção(selecao) {
     document.getElementById('compass-container').style.display = 'none';
     document.getElementById('logs-container').style.display = 'none';
 
     document.getElementById(selecao).style.display = 'block';
+}
+function resetarMensagens() {
+    fetch('/reset', { method: 'POST' })
+        .then(() => {
+            atualizarMensagens();
+            resetarBussola();
+        });
 }
 
 setInterval(atualizarMensagens, 2000); // Atualiza os logs a cada 2s
